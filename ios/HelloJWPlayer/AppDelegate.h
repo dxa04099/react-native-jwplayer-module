cc64b4bbd72149cc8fbd9aa961015289dc56ff00
subhan <dxa04099@gmail.com>
2018-05-22 21:08:23 +0700

    init

---
+++ ios/HelloJWPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloJWPlayer
+//
+//  Created by subhan on 11/30/17.
+//  Copyright © 2017 subhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/HelloJWPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloJWPlayer
+//
+//  Created by subhan on 11/30/17.
+//  Copyright © 2017 subhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
